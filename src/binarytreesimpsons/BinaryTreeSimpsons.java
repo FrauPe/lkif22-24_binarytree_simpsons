68e1f3bbca31972965d8beac2ead5ff130f294b2
Q1.11rothe <Q1.11rothe@R200C04.SGNW.local>
2022-12-22 08:19:33 +0100

    update oder so

---
+++ src/binarytreesimpsons/BinaryTreeSimpsons.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package binarytreesimpsons;
+
+/**
+ * Testklasse zum Aufbau eines BinaryTree, der den Ahnenbaum von Lisa Simpson 
+ * unter Verwendung einer Klasse Ahne darstellt.
+ * @author pardella
+ */
+public class BinaryTreeSimpsons {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final Ahne jacqueline = new Ahne("Jacqueline", "Bouvier", 'w'),
+clancy = new Ahne("Clancy", "Bouvier", 'm'),
+marge = new Ahne("Marge", "Simpson", 'w'),
+mona = new Ahne("Mona", "Simpson", 'w');
+
+    }
+    
+    public static void preorder(BinaryTree pAktWurzel)
+    {
+        //ToDo: Implementiere preorder hier. NEU: Der Teilbaum wird als
+        //Parameter übergeben, es wird nicht auf diesem aufgerufen!
+    }
+    
+    public static void inorder(BinaryTree pAktWurzel)
+    {
+        //ToDo: Implementiere preorder hier. NEU: Der Teilbaum wird als
+        //Parameter übergeben, es wird nicht auf diesem aufgerufen!
+    }
+    
+    public static void postorder(BinaryTree pAktWurzel)
+    {
+        //ToDo: Implementiere preorder hier. NEU: Der Teilbaum wird als
+        //Parameter übergeben, es wird nicht auf diesem aufgerufen!
+    }
+    
+    public static void levelorder(BinaryTree pAktWurzel)
+    {
+        //ToDo: Implementiere preorder hier. NEU: Der Teilbaum wird als
+        //Parameter übergeben, es wird nicht auf diesem aufgerufen!
+    }
+}
